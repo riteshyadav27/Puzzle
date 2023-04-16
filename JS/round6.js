5e40129ffa57252d7fbecb04861f4093836e8bfc
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-04-16 15:27:08 +0530

    Round 6 Done

---
+++ JS/round6.js
@@ -0,0 +1,191 @@
+// declarations
+
+const controlBox = document.getElementById('controlBox')
+const speed = 10
+
+
+main() //Main function starts
+
+async function main() {
+
+    let txt = `Welcome Son of A'Lars and Daughter of Thanos !! I observe that you have colllect all the remaining stones !`
+    setTimeout( ()=> red_skull_dialouge(txt), 1000)
+
+    txt2 = `But this stone is special you need to give up your most precious thing. Thing that you love most !!`
+    setTimeout( ()=> red_skull_dialouge(txt2), 3000)
+
+    txt3 = ` look THANOS it is real close to you !! You only get ONE Chance`
+    setTimeout( ()=> red_skull_dialouge(txt3), 5000)
+
+    setTimeout( ()=> gamora_dialouge(` HA  ! He is a monster He never loved someone ! No Point in Crying now DAD !`), 1000)
+    
+    txt = `The Tears are not for him ! these are for you !!`
+    setTimeout( ()=> red_skull_dialouge(txt3), 5000)
+    
+
+
+}
+
+
+// Show Thanos Objective
+async function red_skull_dialouge( txt ) {
+
+    await dialouge("../Images/round6/red_skull.png").then((res) => {
+
+        controlBox.innerHTML =  res.outerHTML
+
+        console.log("Appended");
+
+        const dialougeCon = document.getElementById('dialouge')
+
+        typeWriter(dialougeCon, txt, 0)
+
+    })
+}
+
+async function  vanish_red_skull(){
+
+    document.getElementById('heroesCon').style.width = "0%"
+
+}
+
+// Show Thanos Objective
+async function gamora_dialouge( txt ) {
+
+    await dialouge("../Images/round6/Gamora_head.png").then((res) => {
+
+        controlBox.innerHTML =  res.outerHTML
+
+        console.log("Appended");
+
+        const dialougeCon = document.getElementById('dialouge')
+
+        typeWriter(dialougeCon, txt, 0)
+
+    })
+}
+
+
+// Creates HTML Body to add Dialouge
+const dialouge = (headSrc) => {
+
+    return new Promise((resolve, reject) => {
+
+        const dialougeCon = document.createElement('div')
+        dialougeCon.className = " dialougeCon "
+
+        const headCon = document.createElement('div')
+        headCon.className = " headCon"
+
+        const head = document.createElement('img')
+        head.className = " dialougeHead "
+        head.src = headSrc
+
+        headCon.appendChild(head)
+
+        const dialouge = document.createElement('p')
+        dialouge.className = " dialouge "
+        dialouge.id = "dialouge"
+
+        dialougeCon.appendChild(headCon)
+        dialougeCon.appendChild(dialouge)
+
+
+        resolve(dialougeCon)
+
+    })
+
+}
+
+//TypeWriter Function
+function typeWriter(textCon, txt, i) {
+
+
+    if (i < txt.length) {
+  
+      textCon.innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(() => { typeWriter(textCon, txt, i) }, speed);
+  
+    }
+  
+  
+  }
+  
+
+
+//Show StoneAcquired Function
+async function stoneAcquired(stoneSrc, stone) {
+
+    //create another section
+    let acquirePage = document.createElement('section')
+    acquirePage.className = "acquirePage"
+  
+    //Add Stone Image
+    let stoneImg = document.createElement('img')
+    stoneImg.src = stoneSrc
+    stoneImg.style.height = "20rem"
+  
+    //Add Title and Msg
+    let title = document.createElement('p')
+    title.textContent = `CONGRATULATIONS !`
+  
+    let msg = document.createElement('p')
+    msg.textContent = `You have acquired ${stone} Stone`
+  
+    //Append All Elements
+    acquirePage.append(title, stoneImg, msg)
+  
+    document.body.innerHTML = acquirePage.outerHTML
+  
+  }
+
+//Scene 1 ==> Thanos Sacrifies Gamora by thowing her off the Cliff
+async function scene1() {
+
+    console.log("In scene 1");
+    //Creating Scene 1
+    let scene = document.createElement('p')
+    scene.id = "dialouge"
+  
+  
+    controlBox.innerHTML = scene.outerHTML
+    let txt = "Thanos Sacrifies Gamora by thowing her off the Cliff"
+  
+    scene = document.getElementById('dialouge')
+  
+    typeWriter(scene, txt, 0)
+    document.getElementById('heroesCon').style.width = "30%"
+  
+  }
+
+
+document.getElementById('gamora').onclick = ()=>{
+
+
+    document.getElementById('thanos').src = "../Images/round6/thanos-crying.gif"
+    scene1()
+
+     setTimeout( ()=>{
+        document.querySelector('.round2-container').innerHTML = "<img id='final_act' src='../Images/round6/thanos-gamora.gif'>" 
+    }, 2000)
+
+    setTimeout( ()=>{ window.location.href = '/HTML/game_over.html' }  , 7000)
+
+
+
+}
+
+document.getElementById('gaunlet').onclick = ()=>{
+
+
+    stoneAcquired( '../Images/Stones/soul_stone.png' , "Soul" )
+
+    setTimeout( ()=>{
+        document.body.innerHTML = "<img id='snaps' src='../Images/round6/thanos-infinity.gif'>"
+      } , 2000 )
+
+    setTimeout(()=>{ window.location.href = '/HTML/snaps.html'  } , 3500 )
+    
+
+}
